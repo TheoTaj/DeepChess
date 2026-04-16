97b7e4b2fefad15d652a54635b9e652999ff2104
TheoTaj <theotajbakhsh@gmail.com>
2026-04-16 18:52:38 +0200

    change loss func for the test for asym

--- src/train_CNN.py
+++ src/train_CNN.py
@@ -22,7 +22,7 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
-def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10, lr=0.01, alpha=1.0):
+def train_CNN(model, train_loader, test_loader, device, model_path, model_name, n_epochs=10, lr=0.01, alpha=1.0):
     
     if str(device) == "cuda" and not torch.cuda.is_available():
         print("CUDA is not available. No training")
@@ -30,9 +30,11 @@ def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10,
 
     if alpha == 1.0: 
         # if alpha is 1, we use MSELoss for computation efficiency. Otherwise we would compare signs for nothing.
-        criterion = nn.MSELoss()
+        criterion_train = nn.MSELoss()
     else:
-        criterion = AsymmetricMSE(alpha)
+        criterion_train = AsymmetricMSE(alpha)
+
+    criterion_test = nn.MSELoss() # for evaluation we want to have comparable metrics.
 
     model_config = model.get_config()
 
@@ -47,7 +49,7 @@ def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10,
     
     run = wandb.init(
         entity="DeepChess",
-        name=f"CNN_100K_10epochs_2",
+        name=model_name,
         project="DeepChess",
         config={**model_config, **train_config}
     )
@@ -65,7 +67,7 @@ def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10,
 
             # forward
             outputs = model(x).squeeze(1)
-            loss = criterion(outputs, y)
+            loss = criterion_train(outputs, y)
 
             # backward
             optimizer.zero_grad() # gradients to zero
@@ -86,7 +88,7 @@ def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10,
             for x, y in test_loader:
                 x, y = x.to(device), y.to(device)
                 outputs = model(x).squeeze(1)
-                loss = criterion(outputs, y)
+                loss = criterion_test(outputs, y)
 
                 same_sign = (outputs * y) > 0 
                 test_correct_sign += same_sign.sum().item()
@@ -112,9 +114,9 @@ def train_CNN(model, train_loader, test_loader, device, model_path, n_epochs=10,
 if __name__== "__main__":
     
     config = {
-        "epochs": 10,
-        "lr": 0.01,
-        "alpha": 1,
+        "epochs": 20,
+        "lr": 0.001,
+        "alpha": 1.5,
         "batch_size": 128,
         "parquet_path": "data/dataset_100000.parquet",
         "conv_filters": [20, 50],
@@ -122,7 +124,7 @@ if __name__== "__main__":
         "fc_layers": [500],
         "dropout": 0.3,
         "activation": nn.ELU,
-        "model_name": "CNN_100k.pth"
+        "model_name": "CNN_100k_asym_0.001", #used for saving the model and for wandb run name
     }
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -151,7 +153,7 @@ if __name__== "__main__":
     )
 
     model.to(device)
-    model_path = f"models/{config['model_name']}"
+    model_path = f"models/{config['model_name']}.pth"
 
     train_CNN(
         model=model,
@@ -159,6 +161,7 @@ if __name__== "__main__":
         test_loader=test_loader,
         device=device,
         model_path=model_path,
+        model_name=config["model_name"],
         n_epochs=config["epochs"],
         lr=config["lr"],
         alpha=config["alpha"]
